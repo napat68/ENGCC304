743b5b0b3fa9b8e351e631ec855483082b83b1d4
napat68 <aoridea13@gmail.com>
2025-07-04 11:34:11 +0700

    Update Lab01 main.c and executable

--- Lab01/main.c
+++ Lab01/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char Name[50];
+    int  Age = 0 ;
+    printf( "Enter your name: " );
+    scanf( "%s", Name );
+    printf( "Enter your age: " );
+    scanf( "%d", &Age );
+    printf( "- - - - - -\n" );
+    printf( "Hello %s \n", Name );
+    printf( "Age = %d\n", Age );
+
     return 0;
-}
+}//end main function
